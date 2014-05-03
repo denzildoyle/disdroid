ed7880ca8e34a0efe08a416f7cf900b434b4954f
Denzil Doyle <doyle.d@hotmail.com>
2014-05-02 22:53:44 -0400

    add swiper

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
+//fade spinner-menu in and out
 $(".spinner").click(function() {
     $( ".spinner-menu" ).fadeToggle();
 });
+
+//swiper
+var mySwiper = new Swiper('.swiper-container',{
+    pagination: '.pagination',
+    loop: true,
+    paginationClickable: true
+})
